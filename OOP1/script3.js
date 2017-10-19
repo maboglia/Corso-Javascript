79a61da9aab9968a304ea677fb852124597004f7
maboglia <mauro.bogliaccino@gmail.com>
2017-10-19 13:46:28 +0200

    array

--- OOP1/script3.js
+++ OOP1/script3.js
@@ -28,8 +28,44 @@ console.log(array3.length);
 
 //4 modo, più utilizzato per costruire un array
 var array4 = [7,77,777];
+array4[10] = 5;
+
 console.log("la prima posizione dell'array 4 vale " + array4[0]);
 console.log(array4.length);
 
 
+var studenti = [   
+		["artur", "holobut", 1992],//cognome posiz studenti[0][2] 
+		["dario", "d'abbiero", 1990],
+		["bryan", "folleco", 1999],
+		["ludovica", "magnano", 1993],
+		["alexandru", "boaca", 1994],
+		["igli", "balliu", 1998],
+		["giuseppe", "minniti", 1995],
+		["simone", "marzorati", 1997],
+		["danut", "andres", 1995],
+		["alberto", "tito", 1990],
+		["andrei", "tasca", 1991],
+		["cristiano", "maffeis", 1995],
+		["marco", "de palma", 1996],
+		["denilson", "vargas", 1992],
+		["daniele", "spada", 1988],
+		["fabio", "caccia", 1997],
+		["davide", "tacchino", 1995]
+
+  ];
+
+
+
+
 //cicli for (posso utilizzarli per scorrere un array)
+
+for (var i = 0; i < studenti.length; i++) {
+	eta = 2017 - (studenti[i][2]);
+	console.log("Lo stud " + studenti[i][1] + " ha circa " + eta);
+
+};
+
+
+
+
