c4976f6b032ed3429d015dc1492c28bc0e1ea4c1
maboglia <mauro.bogliaccino@gmail.com>
2018-01-30 19:24:33 +0100

    array

---
+++ TMWD2018/OOP8/studenti.js
@@ -0,0 +1,21 @@
+var studenti = [
+
+    ["pizzuti", "chiara", 1990, "foto.jpg"],
+    ["gastaldi", "stefano", 1997, "foto.jpg"],
+    ["maggi", "federica", 1992, "foto.jpg"],
+    ["dibenedetto", "giulia", 1990, "foto.jpg"],
+    ["desogus", "mattia", 1992, "foto.jpg"],
+    ["cordova", "tracy", 1991, "foto.jpg"],
+    ["zatari", "miriam", 1994, "foto.jpg"],
+    ["iusso", "andrea", 1990, "foto.jpg"],
+    ["federica", "caiulo", 1987, "foto.jpg"],
+    ["mihoc", "nicolas", 1995, "foto.jpg"],
+    ["colliard", "emily", 1994, "foto.jpg"],
+    ["arico", "paolo", 1987, "foto.jpg"],
+    ["roberta", "toscano", 1991, "foto.jpg"],
+    ["restuccia", "lorenzo", 1993, "foto.jpg"],
+    ["medda", "marco", 1995, "foto.jpg"],
+    ["guddemi", "erika", 1994, "foto.jpg"],
+    ["mameli", "valentina", 1989, "foto.jpg"]
+
+];
